04633998b1fb6056c865d2125c3611bf6925f6dd
Kendall Hudson <64994077+kendallhudson@users.noreply.github.com>
2023-01-21 12:02:00 -0600

    start part 3

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,53 @@
-const express = require('express');
+const express = require("express");
 const postsRouter = express.Router();
 
-const { getAllPosts } = require('../db');
+const { requireUser } = require("./utils");
+
+// From users.js to assist w/ 
+const { getUserByUsername } = require('../db');
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  // Placeholder for testing functionality
+  // res.send({ message: 'under construction' });
+
+  //Part 3 updates
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    const currentUser = await getUserByUsername(username);
+    const id = currentUser.id
+    console.log('currentUser.id =', id);
+    // attempt to define user - from users.js
+    postData = {currentUser, title, content}
+    // add authorId, title, content to postData object
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    if (post) {
+        res.send({ post });
+    // if the post comes back, res.send({ post });
+    } else {
+        next({
+            name: 'PostDataError',
+            message: `Post not created properly`
+        });
+    // otherwise, next an appropriate error object
+    }
+    
+    
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+const { getAllPosts } = require("../db");
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -9,11 +55,11 @@ postsRouter.use((req, res, next) => {
   next(); // THIS IS DIFFERENT
 });
 
-postsRouter.get('/', async (req, res) => {
+postsRouter.get("/", async (req, res) => {
   const posts = await getAllPosts();
   console.log(posts);
   res.send({
-    posts
+    posts,
   });
 });
 
